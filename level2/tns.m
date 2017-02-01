ec41089e642f7d48d3766a7540c14ecd88678121
Savvas Sampaziotis <savvas456pentium@gmail.com>
2017-02-01 14:56:37 +0200

    Dir cleanup

--- level2/tns.m
+++ level2/tns.m
@@ -1,6 +1,23 @@
-function [ output_args ] = tns( input_args )
+function [ frameFout, TNScoeffs ] = TNS( frameFin, frameType )
 %TNS Summary of this function goes here
 %   Detailed explanation goes here
+
+% MDCT coeff Normalisation
+Sw = 1;
+frameFin = frameFin./Sw;
+
+
+% Wiener-Hopf Solution
+
+
+
+
+% Quantization
+
+
+
+% FIR filtering
+
 
 
 end
