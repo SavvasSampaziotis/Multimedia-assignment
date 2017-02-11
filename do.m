805de072746779c73766cd59077270729926a8c0
Savvas Sampaziotis <savvas456pentium@gmail.com>
2017-02-11 18:44:19 +0200

    level 3 minor

--- do.m
+++ do.m
@@ -6,44 +6,9 @@ fNameIn = ['songs\',fName,'.wav'];
 fNameOut = ['exports\',fName,'_SAVVAS_1.wav'];
 %
 snr = demoAAC2(fNameIn, fNameOut);
-
 snr
-% 
-% [AACSeq] = AACoder2();
-% x = iAACoder2(AACSeq, fNameOut);
-% 
-% 
-% load('level3.mat')
-% plot(x-y)
-%  AACSeq1 = AACoder1();
-% load('AACSeq.mat');
-% i = 134;
 
 
-%[Fout,TNS] = TNS(frameF, 'OLS');
-% Fout = iTNS(Fout, 'OLS', TNS);
-
-
-%
-% iTNS test
-%
-% frameF = AACSeq(i).chl.frameF;
-% frameType = AACSeq(i).frameType;
-% if strcmp(frameType, 'ESH') continue; end
-% 
-% TNScoefs = TNSDequantizer(AACSeq(i).chl.TNScoefs);
-% if ~isstable(1,[1;-TNScoefs ])
-%     disp(['[TNS] Filter UNSTABLE ', frameType]);
-%     
-%     H = tf(1, [1;-TNScoefs]', 0.01)
-%     %      [~,P,~,~]= zpkdata(H)
-%     %Hitns = tf(1,[1;a_]);
-% end
-% frameFout = filter(1,[1; - TNScoefs ], frameF);
-% 
-% frameFORIGINAL = AACSeq1(i).chl.frameF;
-
-%Let'w try the reverse filtering...
 
 
 % clf;
---
+++ level3/psycho.m
@@ -0,0 +1,8 @@
+function [ SMR ] = psycho(frameT, frameType, frameTprev1, frameTprev2)
+%PSYCHO Summary of this function goes here
+%   Detailed explanation goes here
+
+
+
+end
+
